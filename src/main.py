957ae3448de04990ca3b66f572b5cc1f2d91e237
Borys <83366051+fxxsx@users.noreply.github.com>
2022-05-20 15:58:56 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-''' run it with python {path} {seconds} in the terminal'''
+''' run it with: python {path} {seconds} in the terminal'''
 import time, subprocess, sys
 import pyautogui as auto
 timeLeft = int("".join(sys.argv[1:]))
@@ -11,7 +11,7 @@ try:
             timeLeft = timeLeft - 1
         subprocess.Popen(['start', proc], shell=True)
 except KeyboardInterrupt:
-    time.sleep(2)
+    time.sleep(0.5)
     auto.alert("Exited the program")
     proc.kill()
     quit()
