58b6f7b237df54964f90eb9d6770283ec894e65a
Borys <83366051+fxxsx@users.noreply.github.com>
2022-05-20 15:51:23 +0200

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,17 @@
+''' run it with python {path} {seconds} in the terminal'''
+import time, subprocess, sys
+import pyautogui as auto
+timeLeft = int("".join(sys.argv[1:]))
+proc = r"enter the path here with double backslashes"
+try:
+    while timeLeft > 0:
+        for i in range(timeLeft + 1):
+            print(timeLeft, end='')
+            time.sleep(1)
+            timeLeft = timeLeft - 1
+        subprocess.Popen(['start', proc], shell=True)
+except KeyboardInterrupt:
+    time.sleep(2)
+    auto.alert("Exited the program")
+    proc.kill()
+    quit()
