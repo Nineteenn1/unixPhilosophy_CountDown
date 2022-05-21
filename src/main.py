bdd104c7d829312d3154544f9ab6de07f3ee8280
Borys <83366051+fxxsx@users.noreply.github.com>
2022-05-21 08:24:36 +0200

    :bulb:Add Comments
    They explain how the program works

--- src/main.py
+++ src/main.py
@@ -3,15 +3,15 @@ I use Windows so you may have to change the code a little bit so it fits your OS
 import time, subprocess, sys
 import pyautogui as auto
 timeLeft = int("".join(sys.argv[1:]))
-proc = r"enter the path here" #use raw input or double backslashes e.g. C:\\ instead of C:\
-try:
-    while timeLeft > 0:
-        for i in range(timeLeft + 1):
-            print(timeLeft, sep='\n')
-            time.sleep(1)
-            timeLeft -= 1
+proc = r"enter the path here" #Use raw input or double backslashes e.g. C:\\ instead of C:\, raw input begins with the prefix r like shown
+try: #Handle the exception
+    while timeLeft > 0: #Repeat the process until the time's up
+        for i in range(timeLeft + 1): #Make it possible to stop the program
+            print(timeLeft, sep='\n') #Print out the time left
+            time.sleep(1) #wait 1 second
+            timeLeft -= 1 #subtract 1 from it
         subprocess.Popen(['start', proc], shell=True)
-except KeyboardInterrupt:
-    time.sleep(0.5)
-    auto.alert("Successfully exited the program")
-    quit()
+except KeyboardInterrupt: #This happens if you press CTRL+C
+    time.sleep(0.5) #Wait 0.5 seconds
+    auto.alert("Successfully exited the program") #Pop up an alert
+    quit() #Exit the program
